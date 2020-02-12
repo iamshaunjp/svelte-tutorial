0dada9ed697b3ba0ceec96861c7d135728003752
Shaun <shaun@thenetninja.co.uk>
2020-02-12 16:47:48 +0000

    lesson-9

--- myproject/src/App.svelte
+++ myproject/src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Modal from './Modal.svelte';
+
 	let people = [
     { name: 'yoshi', beltColour: 'black', age: 25, id: 1 },
     { name: 'mario', beltColour: 'orange', age: 45, id: 2 },
@@ -9,18 +11,9 @@
     people = people.filter(person => person.id != id);
     console.log(e);
   };
-
-  let num = 5;
 </script>
 
-{#if num > 20}
-  <p>Greater than 20</p>
-{:else if num > 5}
-  <p>Greater than 5</p>
-{:else}
-  <p>Not greater than 5</p>
-{/if}
-
+<Modal />
 <main>
   {#each people as person (person.id)}
     <div>
---
+++ myproject/src/Modal.svelte
@@ -0,0 +1,15 @@
+<script>
+  let showModal = true;
+</script>
+
+{#if showModal}
+  <div class="backdrop">
+    <div class="modal">
+      <p>Sign up for offers!</p>
+    </div>
+  </div>
+{/if}
+
+<style>
+
+</style>
