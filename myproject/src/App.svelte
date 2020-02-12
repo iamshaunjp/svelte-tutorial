2a9bafd272be4e668802cc578e23c46ec0bbf10e
Shaun <shaun@thenetninja.co.uk>
2020-02-12 16:24:15 +0000

    lesson-7

--- myproject/src/App.svelte
+++ myproject/src/App.svelte
@@ -4,6 +4,11 @@
     { name: 'mario', beltColour: 'orange', age: 45, id: 2 },
     { name: 'luigi', beltColour: 'brown', age: 35, id: 3 }
   ];
+
+  const handleClick = (e, id) => {
+    people = people.filter(person => person.id != id);
+    console.log(e);
+  };
 </script>
 
 <main>
@@ -11,6 +16,7 @@
     <div>
       <h4>{person.name}</h4>
       <p>{person.age} years old, {person.beltColour} belt.</p>
+      <button on:click={(e) => handleClick(e, person.id)}>delete</button>
     </div>
   {:else}
     <p>There are no people to show...</p>
