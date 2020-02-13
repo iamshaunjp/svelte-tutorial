92958b2ced4c17b72223bc26a04484de1223dc9b
Shaun <shaun@thenetninja.co.uk>
2020-02-13 18:16:04 +0000

    lesson-23

--- ninjapolls/src/components/CreatePollForm.svelte
+++ ninjapolls/src/components/CreatePollForm.svelte
@@ -2,9 +2,36 @@
   import Button from '../shared/Button.svelte';
 
   let fields = { question: '', answerA: '', answerB: '' };
+  let errors = { question: '', answerA: '', answerB: '' };
+  let valid = false;
 
-  let submitHandler = () => {
-    console.log(fields);
+  const submitHandler = () => {
+    valid = true;
+    // question
+    if (fields.question.trim().length < 5) {
+      valid = false;
+      errors.question = 'Question must be at least 5 chars long'
+    } else {
+      errors.question = ''
+    }
+    // answer A
+    if (fields.answerA.trim().length < 1) {
+      valid = false;
+      errors.answerA = 'Answer A cannot be empty'
+    } else {
+      errors.answerA = ''
+    }
+    // answer B
+    if (fields.answerB.trim().length < 1) {
+      valid = false;
+      errors.answerB = 'Answer B cannot be empty'
+    } else {
+      errors.answerB = ''
+    }
+    // add new poll
+    if (valid) {
+      console.log('valid', fields);
+    }
   }
 </script>
 
@@ -12,14 +39,17 @@
   <div class="form-field">
     <label for="question">Poll Question:</label>
     <input type="text" id="question" bind:value={fields.question}>
+    <div class="error">{ errors.question }</div>
   </div>
   <div class="form-field">
     <label for="answer-a">Answer A value:</label>
     <input type="text" id="answer-a" bind:value={fields.answerA}>
+    <div class="error">{ errors.answerA }</div>
   </div>
   <div class="form-field">
     <label for="answer-b">Answer B value:</label>
     <input type="text" id="answer-b" bind:value={fields.answerB}>
+    <div class="error">{ errors.answerB }</div>
   </div>
   <Button>Add Poll</Button>
 </form>
@@ -40,5 +70,10 @@
   label{
     margin: 10px auto;
     text-align: left;
+  }
+  .error{
+    font-weight: bold;
+    font-size: 12px;
+    color: #d91b42;
   }
 </style>
