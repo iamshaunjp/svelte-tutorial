d95a61ce6e99958af2bffdca4efb636a7f176515
Shaun <shaun@thenetninja.co.uk>
2020-02-13 10:11:54 +0000

    lesson-10

--- myproject/src/Modal.svelte
+++ myproject/src/Modal.svelte
@@ -1,9 +1,10 @@
 <script>
   let showModal = true;
+  let isPromo = true;
 </script>
 
 {#if showModal}
-  <div class="backdrop">
+  <div class="backdrop" class:promo={isPromo}>
     <div class="modal">
       <p>Sign up for offers!</p>
     </div>
@@ -11,5 +12,22 @@
 {/if}
 
 <style>
-
+  .backdrop{
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background: rgba(0,0,0,0.8);
+  }
+  .modal{
+    padding: 10px;
+    border-radius: 10px;
+    max-width: 400px;
+    margin: 10% auto;;
+    text-align: center;
+    background: white;
+  }
+  .promo .modal{
+    background: crimson;
+    color: white;
+  }
 </style>
