255d1cd2450162ab742cdf12699fcb0b42391184
Shaun <shaun@thenetninja.co.uk>
2020-02-14 13:03:34 +0000

    lesson-26

--- ninjapolls/src/components/PollDetails.svelte
+++ ninjapolls/src/components/PollDetails.svelte
@@ -1,22 +1,26 @@
 <script>
+  import Card from '../shared/Card.svelte';
+
   export let poll;
 
   // reactive values
   $: totalVotes = poll.votesA + poll.votesB;
 </script>
 
-<div class="poll">
-  <h3>{ poll.question }</h3>
-  <p>Total votes: { totalVotes }</p>
-  <div class="answer">
-    <div class="percent percent-a"></div>
-    <span>{ poll.answerA } ({ poll.votesA } votes)</span>
+<Card>
+  <div class="poll">
+    <h3>{ poll.question }</h3>
+    <p>Total votes: { totalVotes }</p>
+    <div class="answer">
+      <div class="percent percent-a"></div>
+      <span>{ poll.answerA } ({ poll.votesA } votes)</span>
+    </div>
+    <div class="answer">
+      <div class="percent percent-b"></div>
+      <span>{ poll.answerB } ({ poll.votesB } votes)</span>
+    </div>
   </div>
-  <div class="answer">
-    <div class="percent percent-b"></div>
-    <span>{ poll.answerB } ({ poll.votesB } votes)</span>
-  </div>
-</div>
+</Card>
 
 <style>
   h3{
---
+++ ninjapolls/src/shared/Card.svelte
@@ -0,0 +1,12 @@
+<div class="card">
+  <slot></slot>
+</div>
+
+<style>
+  .card{
+    background: #fff;
+    padding: 20px;
+    border-radius: 6px;
+    box-shadow: 0px 2px 4px rgba(0,0,0,0.1);
+  }
+</style>
