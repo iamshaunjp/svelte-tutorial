f18df6a8640e8223ec30c6ca8ff4645623320bfa
Shaun <shaun@thenetninja.co.uk>
2020-02-14 13:33:02 +0000

    lesson-29

--- ninjapolls/src/components/PollDetails.svelte
+++ ninjapolls/src/components/PollDetails.svelte
@@ -7,6 +7,8 @@
 
   // reactive values
   $: totalVotes = poll.votesA + poll.votesB;
+  $: percentA = Math.floor(100 / totalVotes * poll.votesA) || 0;
+  $: percentB = Math.floor(100 / totalVotes * poll.votesB) || 0;
 
   // handling votes
   const handleVote = (option, id) => {
@@ -19,11 +21,11 @@
     <h3>{ poll.question }</h3>
     <p>Total votes: { totalVotes }</p>
     <div class="answer" on:click={() => handleVote('a', poll.id)}>
-      <div class="percent percent-a"></div>
+      <div class="percent percent-a" style="width: {percentA}%"></div>
       <span>{ poll.answerA } ({ poll.votesA } votes)</span>
     </div>
     <div class="answer" on:click={() => handleVote('b', poll.id)}>
-      <div class="percent percent-b"></div>
+      <div class="percent percent-b" style="width: {percentB}%"></div>
       <span>{ poll.answerB } ({ poll.votesB } votes)</span>
     </div>
   </div>
@@ -52,5 +54,18 @@
   span{
     display: inline-block;
     padding: 10px 20px;
+  }
+  .percent{
+    height: 100%;
+    position: absolute;
+    box-sizing: border-box;
+  }
+  .percent-a{
+    background: rgba(217,27,66,0.2);
+    border-left: 4px solid #d91b42;
+  }
+  .percent-b{
+    background: rgba(69,196,150,0.2);
+    border-left: 4px solid #45c496;
   }
 </style>
